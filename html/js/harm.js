b9e1f815f96cd9adb482d96af3b02d70f463ff68
Taller <clever@bk.ru>
2014-12-12 19:44:15 +0300

    improved

--- html/js/harm.js
+++ html/js/harm.js
@@ -23,6 +23,114 @@ var chords
         "Gm" : "G-D-Hb",
     };
 
+function getElementByNoteRight(note) {
+
+  if (!(typeof note == 'string') && !(note instanceof String)) {
+      return undefined;
+  }
+
+  if (note.length > 2) {
+      note = note.substring(0,2);
+  }
+
+  if (note === 'F#' || note === 'Db') {
+      return document.getElementById('F_SHARP');
+  }
+
+  if (note === 'G#' || note === 'Ab') {
+      return document.getElementById('G_SHARP');
+  }
+
+  if (note === 'D#' || note === 'Eb') {
+      return document.getElementById('D_SHARP');
+  }
+
+  if (note === 'C4') {
+      return document.getElementById('C4');
+  }
+
+  if ( (note.charAt(1) != '1' && note.charAt(1) != '2' && note.charAt(1) != '3')
+        ||
+        (note.charAt(0) != 'A' && note.charAt(0) != 'B' && note.charAt(0) != 'C' && note.charAt(0) != 'D'
+        && note.charAt(0) != 'E' && note.charAt(0) != 'F' && note.charAt(0) != 'G' && note.charAt(1) != 'H')) {
+      console.log(note.charAt(0));
+      console.log(note.charAt(1));
+      return undefined;
+  }
+
+
+  return document.getElementById(note);
+}
+
+function getElementByNoteLeft(note, row) {
+
+  if (!(typeof note == 'string') && !(note instanceof String)) {
+      return undefined;
+  }
+
+  if (note.length > 2) {
+      note = note.substring(0,2);
+  }
+
+
+  if (note === 'F#') {
+      return document.getElementById('LF_SHARP_1');
+  }
+
+  if (note.length == 2) {
+      return undefined;
+  }
+
+
+  
+}
+
+function getElementByChordLeft(chord) {
+
+  if (!(typeof chord == 'string') && !(chord instanceof String)) {
+      return undefined;
+  }
+
+
+  if (chord.length > 2) {
+      chord = chord.substring(0,2);
+  }
+
+  if (chord === 'Am') {
+      return document.getElementById('LAm_CHORD_2');
+  }
+
+  if (chord === 'Dm') {
+      return document.getElementById('LDm_CHORD_2');
+  }
+
+  if (chord === 'E7') {
+      return document.getElementById('LE7_CHORD_2');
+  }
+
+  if (chord === 'H7') {
+      return document.getElementById('LH7_CHORD_2');
+  }
+
+  if (chord === 'F') {
+      return document.getElementById('LF_CHORD_3');
+  }
+
+  if (chord === 'C') {
+      return document.getElementById('LC_CHORD_3');
+  }
+
+  if (chord === 'G') {
+      return document.getElementById('LG_CHORD_3');
+  }
+
+  if (chord === 'D') {
+      return document.getElementById('LD_CHORD_3');
+  }
+
+  return undefined;
+}
+
 function play() {
     var v = document.getElementById('melody').value; 
     //alert(v);
