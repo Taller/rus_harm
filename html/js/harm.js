d6deeb7309bdfbc490b2a1cbf781a6ac40ab86df
Taller <clever@bk.ru>
2014-12-13 01:27:49 +0300

    improved

--- html/js/harm.js
+++ html/js/harm.js
@@ -1,144 +1,188 @@
-var chords 
+var chords
     = {
-        "A"  : "A-E-C#",
-        "A7" : "A-E-C#-G#",
-        "Am" : "A-E-C",
-        "H"  : "H-F#-Eb",
-        "H7" : "H-F#-Eb-A",
-        "Hm" : "H-F#-D",
-        "C"  : "C-E-G",
-        "C7" : "C-E-G-Hb",
-        "Cm" : "C-Eb-G",
-        "D"  : "D-A-F#",
-        "D7" : "D-A-F#-C",
-        "Dm" : "D-A-F",
-        "E"  : "E-H-G#",
-        "E7" : "E-H-G#-D",
-        "Em" : "E-H-G",
-        "F"  : "F-C-A",
-        "F7" : "F-C-A-Eb",
-        "Fm" : "F-C-G#",
-        "G"  : "G-D-H",
-        "G7" : "G-D-H-F",
-        "Gm" : "G-D-Hb",
-    };
+    "A": "A-E-C#",
+    "A7": "A-E-C#-G#",
+    "Am": "A-E-C",
+    "H": "H-F#-Eb",
+    "H7": "H-F#-Eb-A",
+    "Hm": "H-F#-D",
+    "C": "C-G-E",
+    "C7": "C-G-E-Hb",
+    "Cm": "C-G-Eb",
+    "D": "D-A-F#",
+    "D7": "D-A-F#-C",
+    "Dm": "D-A-F",
+    "E": "E-H-G#",
+    "E7": "E-H-G#-D",
+    "Em": "E-H-G",
+    "F": "F-C-A",
+    "F7": "F-C-A-Eb",
+    "Fm": "F-C-G#",
+    "G": "G-D-H",
+    "G7": "G-D-H-F",
+    "Gm": "G-D-Hb",
+};
 
-function getElementByNoteRight(note) {
+function getElementByNoteRight(note, octave) {
 
-  if (!(typeof note == 'string') && !(note instanceof String)) {
-      return undefined;
-  }
+    if (!(typeof note == 'string') && !(note instanceof String)) {
+        return undefined;
+    }
 
-  if (note.length > 2) {
-      note = note.substring(0,2);
-  }
+    if (!(typeof octave == 'number')) {
+        return undefined;
+    }
 
-  if (note === 'F#' || note === 'Db') {
-      return document.getElementById('F_SHARP');
-  }
+    if (octave < 1 || octave > 3) {
+        return undefined;
+    }
 
-  if (note === 'G#' || note === 'Ab') {
-      return document.getElementById('G_SHARP');
-  }
+    if (note === 'F#' || note === 'Db') {
+        return document.getElementById('F_SHARP');
+    }
 
-  if (note === 'D#' || note === 'Eb') {
-      return document.getElementById('D_SHARP');
-  }
+    if (note === 'G#' || note === 'Ab') {
+        return document.getElementById('G_SHARP');
+    }
 
-  if (note === 'C4') {
-      return document.getElementById('C4');
-  }
+    if (note === 'D#' || note === 'Eb') {
+        return document.getElementById('D_SHARP');
+    }
 
-  if ( (note.charAt(1) != '1' && note.charAt(1) != '2' && note.charAt(1) != '3')
-        ||
-        (note.charAt(0) != 'A' && note.charAt(0) != 'B' && note.charAt(0) != 'C' && note.charAt(0) != 'D'
-        && note.charAt(0) != 'E' && note.charAt(0) != 'F' && note.charAt(0) != 'G' && note.charAt(1) != 'H')) {
-      console.log(note.charAt(0));
-      console.log(note.charAt(1));
-      return undefined;
-  }
+    if (note === 'C4') {
+        return document.getElementById('C4');
+    }
 
+    if (note.length > 1) {
+        note = note.substring(0, 1);
+    }
 
-  return document.getElementById(note);
+    if ((note != 'A' && note != 'B' && note != 'C' && note != 'D'
+            && note != 'E' && note != 'F' && note != 'G' && note != 'H')) {
+        return undefined;
+    }
+
+    var elementId = note + '' + octave;
+    return document.getElementById(elementId);
 }
 
 function getElementByNoteLeft(note, row) {
+    if (!(typeof note == 'string') && !(note instanceof String)) {
+        return undefined;
+    }
 
-  if (!(typeof note == 'string') && !(note instanceof String)) {
-      return undefined;
-  }
+    if (!(typeof row == 'number')) {
+        return undefined;
+    }
 
-  if (note.length > 2) {
-      note = note.substring(0,2);
-  }
-
-
-  if (note === 'F#') {
-      return document.getElementById('LF_SHARP_1');
-  }
-
-  if (note.length == 2) {
-      return undefined;
-  }
+    if (note.length > 2) {
+        note = note.substring(0, 2);
+    }
 
 
-  
+    if (note === 'F#' || note === 'Gb') {
+        return document.getElementById('LF_SHARP_1');
+    }
+
+    if (note.length == 2) {
+        return undefined;
+    }
+
+    var elementId = 'L' + note + '_' + row;
+    return document.getElementById(elementId);
 }
 
 function getElementByChordLeft(chord) {
 
-  if (!(typeof chord == 'string') && !(chord instanceof String)) {
-      return undefined;
-  }
+    if (!(typeof chord == 'string') && !(chord instanceof String)) {
+        return undefined;
+    }
 
+    if (chord.length > 2) {
+        chord = chord.substring(0, 2);
+    }
 
-  if (chord.length > 2) {
-      chord = chord.substring(0,2);
-  }
+    if (chord === 'Am') {
+        return document.getElementById('LAm_CHORD_2');
+    }
 
-  if (chord === 'Am') {
-      return document.getElementById('LAm_CHORD_2');
-  }
+    if (chord === 'Dm') {
+        return document.getElementById('LDm_CHORD_2');
+    }
 
-  if (chord === 'Dm') {
-      return document.getElementById('LDm_CHORD_2');
-  }
+    if (chord === 'E7') {
+        return document.getElementById('LE7_CHORD_2');
+    }
 
-  if (chord === 'E7') {
-      return document.getElementById('LE7_CHORD_2');
-  }
+    if (chord === 'H7') {
+        return document.getElementById('LH7_CHORD_2');
+    }
 
-  if (chord === 'H7') {
-      return document.getElementById('LH7_CHORD_2');
-  }
+    if (chord === 'F' || chord === 'F7') {
+        return document.getElementById('LF_CHORD_3');
+    }
 
-  if (chord === 'F') {
-      return document.getElementById('LF_CHORD_3');
-  }
+    if (chord === 'C' || chord === 'C7') {
+        return document.getElementById('LC_CHORD_3');
+    }
 
-  if (chord === 'C') {
-      return document.getElementById('LC_CHORD_3');
-  }
+    if (chord === 'G' || chord === 'G7') {
+        return document.getElementById('LG_CHORD_3');
+    }
 
-  if (chord === 'G') {
-      return document.getElementById('LG_CHORD_3');
-  }
+    if (chord === 'D' || chord === 'D7') {
+        return document.getElementById('LD_CHORD_3');
+    }
 
-  if (chord === 'D') {
-      return document.getElementById('LD_CHORD_3');
-  }
-
-  return undefined;
+    return undefined;
 }
 
-function play() {
-    var v = document.getElementById('melody').value; 
-    //alert(v);
-    var notesArr = v.split("-");
-    for (val of notesArr) {
-        console.log(val);
-//        alert(val);
-	document.getElementById(val).className='pressed_as_bold';
+function markNotesFor(chord) {
+    clearPressed();
+
+    var notesInChord = chords[chord].split('-');
+
+    for (note of notesInChord) {
+        for (i = 1; i < 4; i++) {
+            var rightNote = getElementByNoteRight(note, i);
+            if (rightNote != undefined) {
+                setPressedStyle(rightNote);
+            }
+            var leftNote = getElementByNoteLeft(note, i);
+            if (leftNote != undefined) {
+                setPressedStyle(leftNote);
+            }
+            var chord = getElementByChordLeft(chord);
+            if (chord != undefined) {
+                setPressedStyle(chord);
+            }
+        }
+
     }
-    //.className='pressed_as_bold';
+    return notesInChord;
+}
+
+function setPressedStyle(el) {
+    var currentClassName = el.className;
+    if (currentClassName.indexOf('pressed_as_bold') == -1) {
+        el.className = currentClassName + ' pressed_as_bold';
+    }
+}
+
+function removePressedStyle(el) {
+    if (el === undefined) {
+        return;
+    }
+
+    var currentClassName = el.className;
+    if (currentClassName.indexOf('pressed_as_bold') > -1) {
+        el.className = currentClassName.replace(' pressed_as_bold', '');
+    }
+}
+
+function clearPressed() {
+    var pressedElements = document.getElementById('keyboard').getElementsByClassName('pressed_as_bold');
+    while (pressedElements.length > 0) {
+        removePressedStyle(pressedElements[0]);
+    }
 }
